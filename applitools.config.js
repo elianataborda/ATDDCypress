e47ff7e2ef659217be684de44931b2e7a2890f79
elianataborda <margarita320@gmail.com>
2024-04-05 18:54:44 -0500

    second commit

---
+++ applitools.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    testConcurrency: 1,
+    apiKey: 'AfIQGx8IeakRb55omGEU8103x4itByp3YWgBw5Mj1cNIA110',
+    batchName: 'TAU Todo App',
+    browser: [
+        {width: 800, height: 600, name: 'chrome'},
+        {deviceName: 'Pixel 2', screenOrientation: 'portrait'}
+    ]
+}
